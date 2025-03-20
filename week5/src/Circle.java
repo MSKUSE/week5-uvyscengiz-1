dd2565534a2dc0858be0275ca240ffd16e0bfba1
Üveys Cengiz <uveys.cengiz@gmail.com>
2025-03-20 13:14:25 +0300

    wek 5

---
+++ week5/src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+    private Point center;
+    private int radius;
+
+    public Circle(Point center, int radius) {
+        this.center = center;
+        setRadius(radius);
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        if (radius < 0) {
+            this.radius = 0;
+            System.out.println("Radius cannot be negative");
+        } else {
+            this.radius = radius;
+        }
+    }
+}
--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Point p = new Point();
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5);
+
+        Point tLeft = new Point(3, 4);
+        Rectangle r = new Rectangle(tLeft, 3, 4);
+        System.out.println(r.getSideA());
+        System.out.println(r.getSideB());
+        System.out.println(r.getTopLeft());
+
+        Circle c1 = new Circle(
+                new Point(3, 4),
+                5
+        );
+
+        System.out.println(c1.getRadius());
     }
 }
